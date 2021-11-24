91b3fa24a29c6aed873d85834383ae633fbf1432
Valeriy V Nepluev <greenet2007@gmail.com>
2021-11-24 13:01:51 +0300

    feat: add note editable functions - tocode vue 3 course

--- tocode/5 - notes/notes/app.js
+++ tocode/5 - notes/notes/app.js
@@ -7,19 +7,50 @@ const App = {
         value: "",
       },
       notes: ["Task 1", "Task 2", "Task 3"],
+      notesTemp: "",
     };
   },
-  mounted() {},
-  watch: {},
+  mounted() {
+    this.getNotes();
+  },
+  watch: {
+    notes: {
+      handler(updatedList) {
+        localStorage.setItem("notes", JSON.stringify(updatedList));
+      },
+      deep: true,
+    },
+  },
   methods: {
+    getNotes() {
+      const localNotes = localStorage.getItem("notes"); // null
+      if (localNotes) {
+        this.notes = JSON.parse(localNotes);
+      }
+    },
     onSubmit() {
+      localStorage.setItem("notes", this.input.value);
       this.notes.push(this.input.value);
       this.input.value = "";
-      console.log(this.input);
     },
     remove(index) {
       this.notes.splice(index, 1);
       console.log(index);
+    },
+    editNote(event) {
+      this.notesTemp = event.outerText;
+      event.setAttribute("contenteditable", true);
+      event.onselectstart = "return true";
+      event.onblur = (event) => {
+        event.target.textContent = this.notesTemp;
+      };
+    },
+    editNoteEnd(event) {
+      const id = +event.getAttribute("data-id");
+      const text = event.outerText;
+      event.setAttribute("contenteditable", false);
+      this.notes[id] = text;
+      localStorage.setItem("notes", JSON.stringify(this.notes));
     },
   },
 };
