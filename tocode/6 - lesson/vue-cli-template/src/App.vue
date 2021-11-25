aa8d6e36314e943d263d6d1873eb46199c1beba5
Valeriy V Nepluev <greenet2007@gmail.com>
2021-11-25 17:00:21 +0300

    feat: add components Header Footer - tocode vue 3 course

---
+++ tocode/6 - lesson/vue-cli-template/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <div class="wrapper-content">
+      <div class="container">
+        <h1>Hello!</h1>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: { Header, Footer },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ tocode/6 - lesson/vue-cli-template/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "../assets/scss/main.scss";
+
+const App1 = createApp(App);
+
+App1.mount("#app");
