9f4cd61b0e4962c0aaf3f84f4cbbe491ff744700
Valeriy V Nepluev <greenet2007@gmail.com>
2021-11-28 21:36:56 +0300

    feat: implement method OpenNews - vladilen vue 3 course

--- vladilen-traning/lesson 6/src/App.vue
+++ vladilen-traning/lesson 6/src/App.vue
@@ -3,13 +3,16 @@
     <div class="container pt-1">
       <div class="card">
         <h2>Актуальные новости {{ now }}</h2>
+        <span>{{ openRate }}</span>
       </div>
       <app-news
         v-for="item in news"
-        :key="item"
-        :title="item"
+        :key="item.id"
+        :title="item.title"
+        :id="item.id"
+        :is-open="item.isOpen"
+        @open-news="openNews"
       ></app-news>
-
     </div>
   </div>
 </template>
@@ -22,11 +25,30 @@ export default {
   data() {
     return {
       now: new Date().toLocaleDateString(),
+      openRate: 0,
       news: [
-        'На МКС устранили утечку воздуха. Все работает в штатном режиме.',
-        'Россия отмечает международный праздник: QR коды отменены!',
-        'Свой день рождения отмечает наш лучший друг, пригласивший к нему в баню!',
+        {
+          id: 1,
+          title: 'На МКС устранили утечку воздуха. Все работает в штатном режиме.',
+          isOpen: true
+        },
+        {
+          id: 2,
+          title: 'Россия отмечает международный праздник: QR коды отменены!',
+          isOpen: false
+        },
+        {
+          id: 3,
+          title: 'Свой день рождения отмечает наш лучший друг, пригласивший к нему в баню!',
+          isOpen: false
+        }
       ]
+    }
+  },
+  methods: {
+    openNews(data) {
+      this.openRate++;
+      console.log(data); // <open-news>, 42 будет в data
     }
   },
   components: {
--- vladilen-traning/lesson 6/src/AppNews.vue
+++ vladilen-traning/lesson 6/src/AppNews.vue
@@ -1,20 +1,48 @@
 <template>
     <div class="card">
         <h3>{{ title }}</h3>
-        <button class="btn" @click="isOpen = !isOpen">Открыть</button>
-        <p v-if="isOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet earum placeat, ex qui doloribus voluptatum! Repudiandae doloremque porro accusamus asperiores!</p>
+        <button class="btn" @click="open">
+            {{ isNewsOpen ? 'Закрыть' : 'Открыть' }}
+        </button>
+        <p v-if="isNewsOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet earum placeat, ex qui doloribus voluptatum! Repudiandae doloremque porro accusamus asperiores!</p>
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        'title': String,
-        required: true
+        'title': {
+            type: String,
+            required: true
+        },
+        'id': {
+            type: Number,
+            required: true,
+        },
+        'isOpen': {
+            type: Boolean,
+            required: false,
+            default: false
+        }
     },
     data() {
         return {
-            isOpen: false
+            // напрямую нельзя менять значение isOpen в дочернем
+            // компоненте, иначе компонент App не будет знать, что
+            // мы что-то изменили - могут быть баги
+            // Для решения - мы вводим новую переменную
+            isNewsOpen: this.isOpen
+        }
+    },
+    methods: {
+        open() {
+            this.isNewsOpen = !this.isNewsOpen
+            // в компоненте App мы @open-news слушаем это событие
+            // синтаксис как v-on
+            // 06 Vladilen 00:46:02
+            if (this.isNewsOpen) {
+                this.$emit('open-news', 42)
+            }
         }
     }
 }
