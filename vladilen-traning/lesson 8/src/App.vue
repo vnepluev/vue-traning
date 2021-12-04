b33221ce6ffb63e3e7c650dd6ff0a5179748b4ab
Valeriy V Nepluev <greenet2007@gmail.com>
2021-12-04 17:38:37 +0300

    feat: add GET data from Firebase lesson 8 - vladilen vue 3 course

--- vladilen-traning/lesson 8/src/App.vue
+++ vladilen-traning/lesson 8/src/App.vue
@@ -1,13 +1,80 @@
 <template>
   <div class="container">
-    <div class="card">
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
-    </div>
+
+      <div class="form-control">
+        <label for="name">Введите имя:</label>
+        <input id="name" type="text" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+
+    <AppPeopleList
+     :people="people"
+     @load="loadPeople"
+    ></AppPeopleList>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import AppPeopleList from './AppPeopleList.vue';
+
+const baseUrl = 'https://vue-http-firebase-vladilen-default-rtdb.firebaseio.com/people.json';
+
 export default {
+  data() {
+    return {
+      name: '',
+      people: [],
+    }
+  },
+  mounted() {
+    this.loadPeople()
+  },
+  methods: {
+    async createPerson() {
+      const urlParams = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // В запросе ниже указываем данные, отправляемые на сервер
+        // Предварительно преобразовав их в объект JSON
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      }
+
+      // Отправляем запрос на сервер
+      const response = await fetch(baseUrl, urlParams);
+      // Ожидаем поступления данных
+      const firebaseData = await response.json();
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name
+      });
+
+      this.name = '';
+    },
+    async loadPeople() {
+      // Axios возвращает объект, с полем data - которое содержит
+      // ключ = id, значение = объект { firstName: "...", ... }
+      // применяем деструктуризацию и сразу забираем данные в массив data
+      const { data } = await axios.get(baseUrl);
+      this.people = Object.keys(data).map( (key) => {
+        return {
+          id: key,
+          // firstName: data[key].firstName,
+          ...data[key]
+        }
+      })
+    }
+  },
+  components: { AppPeopleList }
 }
 </script>
 
---
+++ vladilen-traning/lesson 8/src/AppPeopleList.vue
@@ -0,0 +1,22 @@
+<template>
+<div v-if="people.length > 0">
+    <div class="card" v-for="person in people" :key="person.id">
+        <h3>{{ person.firstName }}</h3>
+    </div>
+</div>
+<div class="card center" v-else>
+    <h4>Данные еще не загружены</h4>
+    <button class="btn" @click="$emit('load')">Загрузить список</button>
+</div>
+</template>
+
+<script>
+export default {
+    emits: ['load'],
+    props: ['people'],
+}
+</script>
+
+<style lang="sass">
+
+</style>
