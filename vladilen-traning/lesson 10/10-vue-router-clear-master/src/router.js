764362338fe1d98538f39954240eddaab7835153
Valeriy V Nepluev <greenet2007@gmail.com>
2021-12-17 14:39:35 +0300

    feat: lesson 10 done - vladilen vue 3 course

--- vladilen-traning/lesson 10/10-vue-router-clear-master/src/router.js
+++ vladilen-traning/lesson 10/10-vue-router-clear-master/src/router.js
@@ -2,11 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/views/Login.vue';
 import Forget from '@/views/Forget.vue';
 import Dashboard from '@/views/Dashboard.vue';
-import Mail from '@/views/Mail.vue';
+// import Mail from '@/views/Mail.vue';
 import AppEmailBody from '@/components/AppEmailBody.vue';
 import NotFound from '@/views/NotFound.vue';
 
-export default createRouter({
+// эта страница будет распологаться в отдельном чанге
+// сразу будет не вызвана
+// динамическая загрузка (не занимает места в осном коде)
+const Mail = () => import('@/views/Mail.vue');
+
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
@@ -17,13 +22,24 @@ export default createRouter({
     {
       path: '/forget',
       component: Forget,
+      // мета параметры доступные роутеру, называть можем как угодно
+      meta: {
+        cantEnter: true
+      },
+      // можем давать имена роутам, т.е. внутренние названия для роутов
     },
     {
       path: '/dashboard',
+      name: 'home',
       component: Dashboard,
+      // методы вызывается когда мы хотим попасть на страницу
+      beforeEnter() {
+        console.log('beforeEnter');
+      }
     },
     {
       path: '/mail', // mail/2
+      name: 'email',
       component: Mail,
       children: [
         {
@@ -43,3 +59,25 @@ export default createRouter({
   linkActiveClass: 'active',
   linkExactActiveClass: 'active',
 });
+
+// Будет вызван каждый раз при переключении на новую страницу
+// to - куда делаем навигацию
+// from - откуда пришли
+// next - функция позволяющая либо продолжить, либо остановить
+// процесс перехода по роутеру
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach');
+  if (to.meta.cantEnter) {
+    // next('/dashboard'); // либо false - тогда переход будет не доступен
+    next({ name: 'home' });
+  }
+  else next();
+})
+
+// метод вызывается когда мы уже перешли на страницу
+// в нем мы можем собирать какую-то аналитику о том,
+// какие страницы пользователь посещал
+router.afterEach((to, from) => {
+})
+
+export default router;
--- vladilen-traning/lesson 10/10-vue-router-clear-master/src/views/Dashboard.vue
+++ vladilen-traning/lesson 10/10-vue-router-clear-master/src/views/Dashboard.vue
@@ -4,12 +4,24 @@
 
     <p>Тут ты можешь перейти на любую страницу и почитать заранее заготовленные письма :)</p>
 
-    <router-link to="mail">To Mail</router-link>
+    <router-link :to="{name: 'email'}">To Mail</router-link>
   </div>
 </template>
 
 <script>
 export default {
+  beforeRouteEnter() {
+        console.log('beforeRoutEnter');
+  },
+  // если нужно остановить переход на другую страницу
+  beforeRouteLeave(to, from, next) {
+    const answer = confirm('Уверены, что хотите перейти?');
+    if (answer) {
+      next()
+    } else {
+      next(false) // либо можно не вызывать, будет работать одинаково
+    }
+  }
 }
 </script>
 
