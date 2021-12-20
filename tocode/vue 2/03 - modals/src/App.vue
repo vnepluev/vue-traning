aa6294da59e772ca39f23cc132cc7852fd67a978
Valeriy V Nepluev <greenet2007@gmail.com>
2021-12-20 19:27:36 +0300

    feat: start lesson modals - tocode vue 2 course

---
+++ tocode/vue 2/03 - modals/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <div class="wrapper-content">
+      <div class="container">
+        <router-view />
+
+        <button
+         class="btn btnPrimary"
+         @click="modalFirst = true"
+        >First modal</button>
+
+        <!-- first modal -->
+        <Modals
+         v-if="modalFirst"
+         title="First modal"
+         @close="modalFirst = false"
+        />
+
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+import Modals from "@/components/Modal.vue"
+
+export default {
+  data() {
+    return {
+      modalFirst: false,
+    }
+  },
+  components: { Header, Footer, Modals },
+};
+</script>
+
