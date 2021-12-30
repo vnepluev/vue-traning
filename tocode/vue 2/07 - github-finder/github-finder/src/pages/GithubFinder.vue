d4ed1bf8de6e649ef1a6f461017aa4d42e113f34
Valeriy V Nepluev <greenet2007@gmail.com>
2021-12-30 19:57:02 +0300

    feat: start blog nuxt - tocode vue 2 course

--- tocode/vue 2/07 - github-finder/github-finder/src/pages/GithubFinder.vue
+++ tocode/vue 2/07 - github-finder/github-finder/src/pages/GithubFinder.vue
@@ -1,28 +1,93 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
-    <h1>Github-finder page</h1>
+    <section>
+      <div class="container">
+
+        <!-- errors -->
+        <div class="error" v-if="error" style="margin-bottom: 20px;">
+          <p>{{ error }}</p>
+        </div>
+
+        <!-- search -->
+        <search
+          :value="search"
+          placeholder="Type username..."
+          @search="search = $event"
+        />
+
+        <!-- buttons -->
+        <button v-if="!repos" class="btn btnPrimary" @click="getRepos">Search</button>
+        <button v-else class="btn btnPrimary" @click="getRepos">Search again</button>
+
+        <!-- wrapper -->
+        <div class="repos__wrapper" v-if="repos">
+          <div class="repos-item" v-for="repo in repos" :key="repo.id">
+            <div class="repos-info">
+              <a :href="repo.html_url" class="link" target="_blank">{{ repo.name }}</a>
+              <span> ⭐ {{ repo.stargazers_count }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 
 <script>
 import axios from 'axios'
+import search from '@/components/Search.vue'
 
 export default {
-  components: { axios },
+  components: { axios, search },
   data () {
     return {
-
+      search: '',
+      erorr: null,
+      repos: null,
     }
   },
   methods: {
+    getRepos() {
+      // https://api.github.com/users/vnepluev/repos
+      axios
+      .get(`https://api.github.com/users/${this.search}/repos`)
+        .then(res => {
+          this.repos = res.data;
+          this.erorr = null;
+          console.log(res);
+        })
+        .catch(err => {
+          this.erorr = 'Can`t find this user';
+          this.repos = null;
+          console.log(err);
+        })
+    },
   },
   computed: {
-
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+button {
+  margin-top: 20px;
+}
+.repos__wrapper {
+  width: 400px;
+  margin: 30px 0;
+}
+.repos-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 10px 0;
+  border-bottom: 1px solid #dbdbdb;
+}
 </style>
