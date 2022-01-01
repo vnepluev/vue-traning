7c6bbf212217ed9d10bf0f7ca2b38d6766e3c51d
Valeriy V Nepluev <greenet2007@gmail.com>
2022-01-02 00:01:41 +0300

    refactor: some components changes - tocode vue 2 course

--- tocode/vue 2/08 - nuxtjs/blog/components/system/Header.vue
+++ tocode/vue 2/08 - nuxtjs/blog/components/system/Header.vue
@@ -3,7 +3,7 @@
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
-          <div class="logo">Blog</div>
+          <div class="logo"><nuxt-link to="/">Blog</nuxt-link></div>
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link in links" :key="link.title">
               <nuxt-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</nuxt-link>
@@ -20,7 +20,7 @@ export default {
   data () {
     return {
       links: [
-        { title: 'Home', url: '/' },
+        { title: 'Blog', url: '/blog' },
         { title: 'About', url: '/about' },
       ]
     }
@@ -29,6 +29,9 @@ export default {
 </script>
 
 <style lang="scss">
+.logo a {
+  color: #333;
+}
 .navbar-link {
   &.nuxt-link-exact-active {
     color: #4165de;
---
+++ tocode/vue 2/08 - nuxtjs/blog/layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+<!-- https://nuxtjs.org/docs/concepts/views/
+  так же все вызовы в JS throw(error) будут перенаправлены на этот файл
+  в котором нам нужно настроить обработчики событий
+  punctuation.definition.string - кавычки
+-->
+<section class="not-found">
+  <header1 />
+  <div class="container">
+    <div class="not-found__wrapper">
+      <p class="title">Page Not Found!</p>
+      <nuxt-link to="/">Main Page</nuxt-link>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+import Header1 from "~/components/system/Header.vue"
+
+export default {
+  components: { Header1 }
+}
+</script>
+
+<style lang="scss">
+.not-found__wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 80vh;
+  font-size: 26px;
+}
+</style>
