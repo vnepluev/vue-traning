e2ab1de040e344b756a3c905180aedc626e1db9c
Valeriy V Nepluev <greenet2007@gmail.com>
2022-12-18 09:42:25 +0300

    init: noteballs

---
+++ daniell-vue3-composition-api/vue-noteballs/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Notes</router-link> | 
+      <router-link to="/stats">Stats</router-link>
+    </nav>
+  </header>
+
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
