80541b91b2c11b01284b3ceb8142f2b8733a223c
Valeriy V Nepluev <greenet2007@gmail.com>
2022-12-19 13:13:33 +0300

    feat: add view cards list noteballs

--- daniell-vue3-composition-api/vue-noteballs/src/views/ViewNotes.vue
+++ daniell-vue3-composition-api/vue-noteballs/src/views/ViewNotes.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="notes">
-        <div v-for="i in 3" class="card mb-4">
+
+        <div class="card has-background-success-dark p-4 mb-5">
+            <div class="field">
+                <div class="control">
+                    <textarea class="textarea" placeholder="Add new note"></textarea>
+                </div>
+                </div>
+
+                <div class="field is-grouped is-grouped-right">
+                <div class="control">
+                    <button class="button is-link has-background-success">Add New Note</button>
+                </div>
+            </div>
+        </div>
+
+        <div v-for="note in notes" :key="note.id" class="card mb-4">
             <header class="card-header">
                 <button class="card-header-icon" aria-label="more options">
                 <span class="icon">
@@ -10,7 +25,7 @@
             </header>
             <div class="card-content">
                 <div class="content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem velit, perferendis animi laborum aut. Maiores doloribus, blanditiis error totam quam velit eos cupiditate rerum, dolore suscipit neque minima nulla.
+                    {{ note.content }}
                 </div>
             </div>
             <footer class="card-footer">
@@ -20,3 +35,21 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+/*
+    notes
+*/
+const notes = ref([
+    {
+        id: 'id1',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem velit.'
+    },
+    {
+        id: 'id2',
+        content: 'id2, Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem velit.'
+    }
+])
+</script>
