2e3334f5f61ae7a2b68a676b437f5099bbfb275e
Valeriy V Nepluev <greenet2007@gmail.com>
2022-12-21 11:03:52 +0300

    feat: add Note component noteballs

---
+++ daniell-vue3-composition-api/vue-noteballs/src/components/Notes/Note.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card mb-4">
+        <header class="card-header">
+            <button class="card-header-icon" aria-label="more options">
+            <span class="icon">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+            </button>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                {{ note.content }}
+                <div class="has-text-right has-text-grey-light mt-2">
+                    <small>{{ characterLength }}</small>
+                </div>
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item">Edit</a>
+            <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+/*
+    props
+*/
+const props = defineProps({
+    note: {
+        type: Object,
+        required: true
+    }
+})
+
+/**
+ * character length
+ */
+const characterLength = computed(() => {
+    let length = props.note.content.length
+    let description = length > 1 ? 'characters' : 'character'
+    return `${ length } ${ description }`
+})
+</script>
